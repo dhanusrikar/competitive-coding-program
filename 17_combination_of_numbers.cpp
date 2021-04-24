59172da8d4857c2067c4d79d011df8834a794293
dhanusrikar <42804008+dhanusrikar@users.noreply.github.com>
2021-04-24 23:00:36 +0530

    Update and rename combination_of_numbers.cpp to 17_combination_of_numbers.cpp

--- 17_combination_of_numbers.cpp
+++ 17_combination_of_numbers.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Find the nuber of ways to reach 'x' using n1, n2, n3 and nN numbers
+// Find the number of ways to reach 'x' using n1, n2, n3 and nN numbers
 // Ex: find the number of ways to reach 15 using 3s, 5s, and 10s.
 // We use Dynamic Programming, where the dp table consists of all the numbers upto 'x'
 //  we initialise the first element 0 to 1 and its logic is that no way is also a way.
