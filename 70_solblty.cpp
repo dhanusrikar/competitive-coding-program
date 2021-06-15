ff8585bb9fb0727083e0867eec7b6a5d61c0b813
dhanusrikar <42804008+dhanusrikar@users.noreply.github.com>
2021-06-15 13:29:29 +0530

    Create 70_solblty.cpp

---
+++ 70_solblty.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+int main()
+{
+    fastio
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, a, b;
+        cin >> x >> a >> b;
+        ll ans;
+        ans = a + ((100 - x) * b);
+        cout << ans * 10 << endl;
+    }
+    return 0;
+}
