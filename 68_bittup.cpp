0d7009505cfea5ca2560fa40f1ad1498fefcd6f7
dhanusrikar <42804008+dhanusrikar@users.noreply.github.com>
2021-06-15 13:27:23 +0530

    Create 68_bittup.cpp

---
+++ 68_bittup.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+ll mod = 1e9 + 7;
+using namespace std;
+
+int power(long long x, unsigned int y, int p)
+{
+    int res = 1; // Initialize result
+
+    x = x % p; // Update x if it is more than or
+               // equal to p
+
+    if (x == 0)
+        return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+int main()
+{
+    fastio
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m, count = 0;
+        cin >> n >> m;
+        ll x = power(2, n, mod);
+        // cout << x << endl;
+        x = (x - (1 % mod)) % mod;
+        ll y = power(x, m, mod);
+        cout << y << endl;
+    }
+
+    // cout << "count: " << count;
+    return 0;
+}
