8c7d3c81ac3f0dcc47958f7b983380c005f502fe
dhanusrikar <42804008+dhanusrikar@users.noreply.github.com>
2021-07-13 14:47:09 +0530

    Create 0101.cpp
    // Sum of two numbers without using arithmetic operations

---
+++ 0101.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+ll mod = 1e9 + 7;
+using namespace std;
+
+int sum(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return sum(a ^ b, (a & b) << 1);
+}
+
+int main()
+{
+    fastio
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << sum(3,4);
+    return 0;
+}
